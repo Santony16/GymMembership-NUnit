832a56972037abd39013cca9a5ea51415c32dad6
Santony16 <anthonysancho41@gmail.com>
2026-02-28 21:55:34 -0600

    Add AAA-structured unit tests for PointsConfiguration
    Coverage includes:
    - Validation of default configuration values
    - Updating PointsPerUse
    - Validation rules for invalid configuration inputs
    - Exception handling for invalid minimum redemption values
    
    Tests reflect the current configuration logic.

---
+++ GymSystemTests/PointsConfigurationTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using GymMembershipManagementSystem.Business;
+using System;
+
+namespace GymMembershipManagementSystem.Tests
+{
+    [TestFixture]
+    public class PointsConfigurationTests
+    {
+        [Test]
+        public void DefaultValues_ShouldBeCorrect()
+        {
+            // Arrange
+            var config = new PointsConfiguration();
+
+            // Act
+            int pointsPerUse = config.PointsPerUse;
+            int minimumPoints = config.MinimumPointsToRedeem;
+
+            // Assert
+            Assert.AreEqual(10, pointsPerUse);
+            Assert.AreEqual(50, minimumPoints);
+        }
+
+        [Test]
+        public void SetPointsPerUse_ShouldUpdateValue()
+        {
+            // Arrange
+            var config = new PointsConfiguration();
+            int newValue = 20;
+
+            // Act
+            config.SetPointsPerUse(newValue);
+
+            // Assert
+            Assert.AreEqual(newValue, config.PointsPerUse);
+        }
+
+        [Test]
+        public void SetPointsPerUse_ShouldThrowException_WhenInvalid()
+        {
+            // Arrange
+            var config = new PointsConfiguration();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                config.SetPointsPerUse(0));
+        }
+
+        [Test]
+        public void SetMinimumPointsToRedeem_ShouldThrowException_WhenInvalid()
+        {
+            // Arrange
+            var config = new PointsConfiguration();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                config.SetMinimumPointsToRedeem(0));
+        }
+    }
+}
