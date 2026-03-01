4445041e96f28cdcf96304ca1457453fea42e88c
Karen-2000 <karen2000pje@gmail.com>
2026-02-28 23:54:21 -0600

    Application mocks and editing code for this

---
+++ GymMembershipManagementSystem/Business/IMembershipRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymMembershipManagementSystem.Business
+{
+    public interface IMembershipRepository
+    {
+        void Add(Membership membership);
+        Membership GetById(Guid id);
+        List<Membership> GetAll();
+    }
+}
---
+++ GymMembershipManagementSystem/Business/InMemoryMembershipRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GymMembershipManagementSystem.Business
+{
+    public class InMemoryMembershipRepository : IMembershipRepository
+    {
+        private readonly List<Membership> _memberships = new List<Membership>();
+
+        public void Add(Membership membership)
+        {
+            _memberships.Add(membership);
+        }
+
+        public Membership GetById(Guid id)
+        {
+            var membership = _memberships.FirstOrDefault(m => m.Id == id);
+            if (membership == null)
+                throw new Exception("Membership not found");
+            return membership;
+        }
+
+        public List<Membership> GetAll()
+        {
+            return _memberships;
+        }
+    }
+}
